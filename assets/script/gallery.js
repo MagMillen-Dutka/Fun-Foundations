b7ea4ae5cfd7ce6ed4a4911f378f02f39fa24375
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2024-01-09 21:29:09 +0000

    change of CSS for gallery page - fixed modal

--- assets/script/gallery.js
+++ assets/script/gallery.js
@@ -1,33 +1,29 @@
 function myFunction() {
-    var x = document.getElementById("navigation");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById("navigation");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
   }
-  
-  // JavaScript for modal functionality
-  const images = document.querySelectorAll('.myImg');
-  const modals = document.querySelectorAll('.modal');
-  
-  images.forEach((image, index) => {
-    image.addEventListener('click', () => {
-      modals[index].style.display = 'block';
-    });
-  
-    const closeModal = modals[index].querySelector('.close');
-    closeModal.addEventListener('click', () => {
-      modals[index].style.display = 'none';
-    });
-  
-    window.addEventListener('click', (e) => {
-      if (e.target === modals[index]) {
-        modals[index].style.display = 'none';
-      }
-    });
+}
+
+// JavaScript for modal functionality
+const images = document.querySelectorAll('.myImg');
+const modals = document.querySelectorAll('.modal');
+
+images.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    modals[index].style.display = 'block';
   });
-  
 
+  const closeModal = modals[index].querySelector('.close');
+  closeModal.addEventListener('click', () => {
+    modals[index].style.display = 'none';
+  });
 
-
+  window.addEventListener('click', (e) => {
+    if (e.target === modals[index]) {
+      modals[index].style.display = 'none';
+    }
+  });
+});
